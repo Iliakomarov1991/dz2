e02457d0a380b1e9c58aab89afdf64d681bd33f3
Ilia <Ilia@DESKTOP-ISRQ653>
2022-02-17 01:36:18 +0200

    Добавьте файлы проекта.

---
+++ dz1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int summ = 52;
+            int row = 3;
+            Console.WriteLine("Count of rows: "+summ/row);
+            Console.WriteLine("Paints left: " + summ % row);
+
+
+        }
+    }
+}
